f9330682df6af1e6f6690d02e495dccbf5263c17
Simon Holthe Hansen <shh@email.24sevenoffice.com>
2022-03-02 14:40:03 +0100

    Initial commit

---
+++ MinimalWebApi/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalWebApi.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double? UselessInfo { get; set; }
+    }
+}
---
+++ MinimalWebApi/Models/ItemDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalWebApi.Models
+{
+    public class ItemDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public ItemDTO() { }
+        public ItemDTO(Item item) =>
+        (Id, Name) = (item.Id, item.Name);
+    }  
+}
